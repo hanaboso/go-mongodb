70ba1cb6eedda9c0daaffdb382d5b8f94ae541f6
Radek Bruha <bruha.r@hanaboso.com>
2020-02-18 14:31:37 +0100

    Create MongoDB package

---
+++ connection.go
@@ -0,0 +1,86 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/connstring"
+	"sort"
+	"time"
+)
+
+const errorFormat = "[MongoDB] %+v"
+
+// Connection represents MongoDB connection
+type Connection struct {
+	Database *mongo.Database
+	timeout  time.Duration
+}
+
+// Connect to MongoDB
+func (connection *Connection) Connect(dsn string) {
+	connectionString, err := connstring.Parse(dsn)
+
+	if err != nil {
+		fmt.Println(fmt.Sprintf(errorFormat, err))
+		connection.Connect(dsn)
+
+		return
+	}
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(dsn))
+
+	if err != nil {
+		fmt.Println(fmt.Sprintf(errorFormat, err))
+		connection.Connect(dsn)
+
+		return
+	}
+
+	if err := client.Connect(context.Background()); err != nil {
+		fmt.Println(fmt.Sprintf(errorFormat, err))
+		connection.Connect(dsn)
+
+		return
+	}
+
+	if err := client.Ping(context.Background(), nil); err != nil {
+		fmt.Println(fmt.Sprintf(errorFormat, err))
+		connection.Connect(dsn)
+
+		return
+	}
+
+	connection.Database = client.Database(connectionString.Database, nil)
+	connection.timeout = getTimeout(connectionString)
+}
+
+// Context creates context with timeout from MongoDB connection string
+func (connection *Connection) Context() (context.Context, context.CancelFunc) {
+	return context.WithTimeout(context.Background(), connection.timeout)
+}
+
+// Disconnect from MongoDB
+func (connection *Connection) Disconnect() {
+	err := connection.Database.Client().Disconnect(context.Background())
+
+	if err != nil {
+		fmt.Println(fmt.Sprintf(errorFormat, err))
+		connection.Disconnect()
+
+		return
+	}
+}
+
+func getTimeout(connectionString connstring.ConnString) time.Duration {
+	timeouts := []int{
+		int(connectionString.ConnectTimeout.Milliseconds()),
+		int(connectionString.SocketTimeout.Milliseconds()),
+		int(connectionString.ServerSelectionTimeout.Milliseconds()),
+	}
+
+	sort.Ints(timeouts)
+
+	return time.Duration(timeouts[len(timeouts)-1]) * time.Millisecond
+}
