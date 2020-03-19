4836ad5b5222d7eea9010e23a7ea82a9acd2b5f2
David <h.raven@centrum.cz>
2020-03-19 12:25:12 +0100

    Adds logger package

--- connection.go
+++ connection.go
@@ -2,50 +2,62 @@ package mongodb
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"sort"
 	"time"
 
+	"github.com/hanaboso/go-log/pkg/zap"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/connstring"
-)
 
-const errorFormat = "[MongoDB] %+v"
+	log "github.com/hanaboso/go-log/pkg"
+)
 
 // Connection represents MongoDB connection
 type Connection struct {
 	Database *mongo.Database
 	timeout  time.Duration
+	log      log.Logger
 }
 
 // Connect creates MongoDB connection
 func (connection *Connection) Connect(dsn string) {
+	if connection.log == nil {
+		connection.log = zap.NewLogger()
+	}
+
 	connectionString, err := connstring.Parse(dsn)
 
 	if err != nil {
-		panic(fmt.Sprintf(errorFormat, err))
+		connection.log.WithFields(map[string]interface{}{
+			"package": "MongoDB",
+		}).Error(err)
 	}
 
 	client, err := mongo.NewClient(options.Client().ApplyURI(dsn))
 
 	if err != nil {
-		log.Println(fmt.Sprintf(errorFormat, err))
+		connection.log.WithFields(map[string]interface{}{
+			"package": "MongoDB",
+		}).Error(err)
 		connection.Connect(dsn)
 
 		return
 	}
 
 	if err := client.Connect(context.Background()); err != nil {
-		log.Println(fmt.Sprintf(errorFormat, err))
+		connection.log.WithFields(map[string]interface{}{
+			"package": "MongoDB",
+		}).Error(err)
 		connection.Connect(dsn)
 
 		return
 	}
 
 	if err := client.Ping(context.Background(), nil); err != nil {
-		log.Println(fmt.Sprintf(errorFormat, err))
+		connection.log.WithFields(map[string]interface{}{
+			"package": "MongoDB",
+		}).Error(err)
 		connection.Connect(dsn)
 
 		return
@@ -60,7 +72,9 @@ func (connection *Connection) Disconnect() {
 	err := connection.Database.Client().Disconnect(context.Background())
 
 	if err != nil {
-		log.Println(fmt.Sprintf(errorFormat, err))
+		connection.log.WithFields(map[string]interface{}{
+			"package": "MongoDB",
+		}).Error(err)
 		connection.Disconnect()
 
 		return
